4116fa6ac3242c67848c98a447239fef88696966
alf27048 <analaurafarfan@gmail.com>
2021-07-17 11:40:18 -0300

    Creamos card un div de cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,26 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Prueba';
   subtitle = 'Todo lo que necesitas saber';
+  cards =[
+    {imagen: "/assets/images/patoruzito.jpg",
+    categoria: "Vuelos",
+    titulo: "Australia",
+    precio: 97997
+  },
+  {imagen: "/assets/images/patoruzito.jpg",
+    categoria: "Paseos",
+    titulo: "Melbourne",
+    precio: 22222
+  },
+  {imagen: "/assets/images/patoruzito.jpg",
+    categoria: "Alojamiento",
+    titulo: "Austin",
+    precio: 3333
+  },
+  {imagen: "/assets/images/patoruzito.jpg",
+    categoria: "Alquiler Autos",
+    titulo: "Medelin",
+    precio: 44444
+  }
+  ]
 }
